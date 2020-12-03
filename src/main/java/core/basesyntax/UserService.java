14d75efa7309524a259055eeec034f7e0bedb5d7
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-03 15:11:31 +0200

    first commit impl task

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,30 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+
+        int userScore = -1;
+
+        if (records == null) {
+            throw new UserNotFoundException("User with given email doesn't exist");
+
+        } else {
+            for (String record : records) {
+                String[] userRecord = record.split(":");
+
+                if (email.equals(userRecord[0])) {
+                    userScore = Integer.valueOf(userRecord[1]);
+                }
+            }
+
+        }
+
+        if (userScore < 0) {
+            throw new UserNotFoundException("User with given email doesn't exist");
+        }
+        return userScore;
     }
 }
