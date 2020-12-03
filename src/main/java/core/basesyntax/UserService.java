c594b260fc91c30f67aeecdaee346285faf8f4da
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-03 16:43:11 +0200

    fidex discussed notation

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,25 +6,15 @@ public class UserService {
 
     public int getUserScore(String[] records, String email) {
 
-        int userScore = -1;
+        for (String record : records) {
+            String[] userRecord = record.split(":");
 
-        if (records == null) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-
-        } else {
-            for (String record : records) {
-                String[] userRecord = record.split(":");
-
-                if (email.equals(userRecord[0])) {
-                    userScore = Integer.valueOf(userRecord[1]);
-                }
+            if (email.equals(userRecord[0])) {
+                return Integer.valueOf(userRecord[1]);
             }
-
         }
 
-        if (userScore < 0) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return userScore;
+        throw new UserNotFoundException("User with given email doesn't exist");
+
     }
 }
